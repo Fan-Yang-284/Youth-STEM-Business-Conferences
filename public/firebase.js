741bc6c3678022cc32d798395611f40a5a9f23e4
Fan-Yang-284 <69977136+Fan-Yang-284@users.noreply.github.com>
2022-04-02 00:07:26 -0400

    Added files

---
+++ public/firebase.js
@@ -0,0 +1,5 @@
+//push on form submit
+// document.getElementById("submitForm").addEventListener('click', function () {
+//     alert('Clicked');
+//     writeUserData(new Date().getTime()/1000,inputEmail);
+// })
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+    },
+    fontSize: {
+      '12xl':"8rem",
+    }
+  },
+  plugins: [],
+}
